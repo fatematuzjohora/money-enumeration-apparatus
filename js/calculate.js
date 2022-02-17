d11da0c87cd2603547b989a2a73ecdcf842c36b3
fatematuzjohora <fatematuzjohora889@gmail.com>
2022-02-17 19:06:49 +0600

    new code

---
+++ js/calculate.js
@@ -0,0 +1,53 @@
+function getElement(id) {
+    const allCostingInput = document.getElementById(id).value;
+    return allCostingInput;
+
+    // error handle
+
+    if (allCostingInput == '') {
+        return alert('please fill the all criteria')
+    }
+    else if (allCostingInput < 0) {
+        return alert('please give positive value')
+
+    }
+    else {
+        isNAN(allCostingInput)
+        return alert('please give floating number')
+
+    }
+    // clear input
+
+
+}
+
+// ---------costing------------
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const userIncome = getElement('total-income');
+    const totalOutlay = document.getElementById('total-outlay');
+    const foodCost = getElement('food-cost');
+    const rentCost = getElement('rent-cost');
+    const clothCost = getElement('cloth-cost');
+    const allItemCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothCost);
+    totalOutlay.innerText = allItemCost;
+
+    const newBalance = document.getElementById('new-balance');
+    const restBalance = userIncome - allItemCost;
+    newBalance.innerText = restBalance;
+    console.log(restBalance);
+
+});
+// saving part
+document.getElementById('save-btn').addEventListener('click', function () {
+    const incomeInput = getElement('total-income');
+    const savingInput = getElement('save-info');
+    const savedBalance = (incomeInput * savingInput) / 100;
+    const recentSaving = document.getElementById('recently-save');
+    recentSaving.innerText = savedBalance;
+    const previousBalance = document.getElementById('new-balance').innerText;
+    const remainingBalance = document.getElementById('remaining-balnace');
+    const updateBalance = previousBalance - savedBalance;
+    remainingBalance.innerText = updateBalance;
+
+})
